4ea6a7ab8ca69902483c6bbe91f56919437681c2
Maria Yuzba <mariayuzba@Maria.local>
2025-03-01 19:35:04 +0000

    updated footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,21 @@
+import styles from "./styles/Footer.module.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
 export default function Footer() {
     return (
         <>
-            <p>Contacts</p>
+            <footer>
+                <p>@ 2025 AirbNC by Maria Yuzba</p>
+                <a 
+                    href="https://github.com/yuzbamaria" 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                    style={{ color: '#007bff', textDecoration: 'none' }}
+                >
+                    <FontAwesomeIcon icon={faGithub} className={styles.gitHubIcon}/>
+                </a>
+            </footer>
         </>
     )
 };
