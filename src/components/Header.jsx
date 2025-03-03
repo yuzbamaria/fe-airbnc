6acf2437b949c8ebcc715a718859a21d3a2695da
Maria Yuzba <mariayuzba@Maria.local>
2025-03-03 09:59:47 +0000

    added functionality for toggling mobile menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,25 +2,40 @@ import styles from "./styles/Header.module.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { Link } from "react-router-dom"; 
+import { useState } from "react";
 
 export default function Header() {
-    // const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    function toggleMobileMenu() {
+        setIsMenuOpen(!isMenuOpen);
+    }
 
     return (
         <>
             <header className={styles.header}>
                 <nav className={styles.nav}>
-                    <button className={styles.menuIcon} aria-label="Toggle navigation">
+                    <button 
+                        onClick={toggleMobileMenu}
+                        className={styles.menuIcon} 
+                        aria-label="Toggle navigation"
+                    >
                         <FontAwesomeIcon icon={faBars} />
                     </button>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/contacts">Contacts</Link>
-                        </li>
-                    </ul>
+                    {isMenuOpen && (
+                        <>
+                            <div 
+                                className={styles.overlay} 
+                                onClick={toggleMobileMenu} 
+                                aria-label="Close Menu"
+                            >
+                                <ul className={styles.open} onClick={(e) => e.stopPropagation()}>
+                                    <li><Link to="/" className={styles.navLink}>Home</Link></li>
+                                    <li><Link to="/contacts" className={styles.navLink}>Contacts</Link></li>
+                                </ul>
+                            </div>
+                        </>
+                    )}
                     <h1>AirbNC</h1>
                     <ul>
                         <li>Guest</li>
@@ -31,18 +46,3 @@ export default function Header() {
         </>
     );
 }
-
- {/* <ul class="nav-links">
-                    <li>
-                        <a href="#home">Home</a>
-                    </li>
-                    <li>
-                        <a href="#about">Another</a>
-                    </li>
-                    <li>
-                        <a href="#services">Another</a>
-                    </li>
-                    <li>
-                        <a href="#contact">Contact</a>
-                    </li>
-                    </ul> */}
