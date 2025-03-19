fa930d061e2271d56a3806cde51cff21e4575fde
Maria Yuzba <mariayuzba@Maria.local>
2025-03-19 13:52:51 +0000

    updated CSS for Booking Confirmation component

--- src/components/BookingConfirmation.jsx
+++ src/components/BookingConfirmation.jsx
@@ -1,11 +1,13 @@
 import { useSearchParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import styles from "./styles/BookingConfirmation.module.css";
+import { useUser } from "../contexts/UserContext";
 
 export default function BookingConfirmation() {
     const [searchParams] = useSearchParams();
     const msg = searchParams.get("msg");
-    const bookingId = searchParams.get("booking_id")
+    const bookingId = searchParams.get("booking_id");
+    const { guestId } = useUser();
 
     return (
         <>
@@ -13,9 +15,15 @@ export default function BookingConfirmation() {
                 <section className={styles.bookingConfirmation}>
                     <p>{msg}. Enjoy your stay!</p>
                     <p>Your booking ID: {bookingId}.</p>
-                    <Link to={`/`} className={styles.homeLink}>
-                        Return to Home page
-                    </Link>
+                    <div className={styles.linksContainer}>
+                        <Link to={`/`} className={styles.homeLink}>
+                            Return to Home page
+                        </Link>
+                        <Link to={`/users/${guestId}`} className={styles.homeLink}>
+                            See all your bookings
+                        </Link>
+                    </div>
+                    
                 </section>
             </main>
         </>
