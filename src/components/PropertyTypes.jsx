bbbd6ba0441946c94191be4f259577e662cc2c0e
Maria Yuzba <mariayuzba@Maria.local>
2025-03-24 14:03:33 +0000

    updated icons fo property types

--- src/components/PropertyTypes.jsx
+++ src/components/PropertyTypes.jsx
@@ -1,20 +1,25 @@
-import { useState } from "react";
 import styles from "./styles/PropertyTypes.module.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse } from '@fortawesome/free-solid-svg-icons';
 
 export default function PropertyTypes({ propertyType }) {
     return (
         <>  
-            {propertyType.map(type => (
-                <button 
-                    key={type} 
-                    className={styles.propertyTypeBtn}
-                >
-                    <FontAwesomeIcon icon={faHouse} className={styles.propertyTypeIcon}/>
-                    {type}
-                </button>
-            ))}
+            {propertyType.map(type => {
+                const imagePath = `/images/${type.toLowerCase()}.png`;
+                return (
+                    <button 
+                        key={type} 
+                        className={styles.propertyTypeBtn}
+                    >
+                        <img 
+                            src={imagePath} 
+                            alt={type} 
+                            className={styles.propertyTypeIcon} 
+                        />
+                        {type}
+                    </button>
+                )
+                
+            })}
         </>
     ) 
 };
